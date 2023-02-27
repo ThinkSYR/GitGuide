7daab246db48b511cc067cda3db20545ddbd9715
hsy <1119613598@qq.com>
2023-02-27 20:08:18 +0800

    v0.1

--- test.py
+++ test.py
@@ -2,4 +2,5 @@ import os
 import sys
 print("Test Git")
 print(os.getcwd())
-print(os.path.dirname(os.path.abspath(__file__)))
+print(os.path.dirname(os.path.abspath(__file__))) # set
+print("v0.1")
